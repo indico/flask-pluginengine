75692775dfc93af8abb357156e355fb77f516d60
Pedro Ferreira <pedro.ferreira@cern.ch>
2014-09-26 11:26:05 +0200

    Configure 'test' command to run py.test

--- setup.py
+++ setup.py
@@ -4,7 +4,28 @@
 # Flask-PluginEngine is free software; you can redistribute it
 # and/or modify it under the terms of the Revised BSD License.
 
+import sys
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -22,4 +43,6 @@ setup(
         'Flask>=0.7',
         'blinker'
     ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 )
