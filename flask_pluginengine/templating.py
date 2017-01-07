384d6ca3bf7c744f1542967155769f034b9d6be6
Adrian Moennich <adrian.moennich@cern.ch>
2017-01-07 15:48:36 +0100

    Happy new year!

--- flask_pluginengine/templating.py
+++ flask_pluginengine/templating.py
@@ -1,5 +1,5 @@
 # This file is part of Flask-PluginEngine.
-# Copyright (C) 2014-2016 CERN
+# Copyright (C) 2014-2017 CERN
 #
 # Flask-PluginEngine is free software; you can redistribute it
 # and/or modify it under the terms of the Revised BSD License.
