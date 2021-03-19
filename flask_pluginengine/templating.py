e2b7e0b63792fdb7dd6895a19ec3ae9584b4ea35
Adrian Moennich <adrian.moennich@cern.ch>
2021-03-19 13:14:50 +0100

    We have 2021

--- flask_pluginengine/templating.py
+++ flask_pluginengine/templating.py
@@ -1,5 +1,5 @@
 # This file is part of Flask-PluginEngine.
-# Copyright (C) 2014-2017 CERN
+# Copyright (C) 2014-2021 CERN
 #
 # Flask-PluginEngine is free software; you can redistribute it
 # and/or modify it under the terms of the Revised BSD License.
