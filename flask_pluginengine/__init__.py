d578087765523dfd925ac39318ad6152e75a8031
Adrian Moennich <adrian.moennich@cern.ch>
2023-10-09 16:48:56 +0200

    Bump version to 0.5

--- flask_pluginengine/__init__.py
+++ flask_pluginengine/__init__.py
@@ -14,7 +14,7 @@ from .templating import PluginPrefixLoader
 from .util import plugin_context, trim_docstring, with_plugin_context, wrap_in_plugin_context
 
 
-__version__ = '0.4.1'
+__version__ = '0.5'
 __all__ = ('PluginEngine', 'current_plugin', 'PluginBlueprintSetupState', 'PluginBlueprintSetupStateMixin',
            'PluginBlueprint', 'PluginBlueprintMixin', 'PluginFlask', 'PluginFlaskMixin', 'Plugin', 'uses', 'depends',
            'render_plugin_template', 'url_for_plugin', 'plugins_loaded', 'PluginPrefixLoader', 'with_plugin_context',
